8799194847aafda4d00f79fa75c8361b5ae686f7
Julian <julianete4@gmail.com>
2023-06-06 20:21:56 -0300

    stats

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store.js'
 import { router } from './pages/index.js'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <Provider store={store}>
         <RouterProvider router={router} />
+        <Analytics />
     </Provider>
   );
 }
